83750256e3623252e20e845f881de45ca4a9560a
SanteriAlakyla <santeri.alakyla@gmail.com>
2015-03-21 16:13:32 +0200

    Leffat ja teatterit

--- app/src/main/java/com/example/matti/lippuvaraaja/TeatteriFragment.java
+++ app/src/main/java/com/example/matti/lippuvaraaja/TeatteriFragment.java
@@ -5,14 +5,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Sami on 19.3.2015.
  */
 public class TeatteriFragment extends Fragment {
+    ListView listView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teatteri, container, false);
+        View teatteriFragmentView = inflater.inflate(R.layout.fragment_teatteri, container, false);
+        listView = (ListView) teatteriFragmentView.findViewById(android.R.id.list);
+
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Paimio International",
+                "Kaarina RockCity",
+                "East side best side"
+
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_titanic, R.id.firstLine, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition     = position;
+
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getActivity().getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+
+        });
+        return teatteriFragmentView;
+
     }
 }
